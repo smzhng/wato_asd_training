b63b84d08375a54aeacf10166c4fc7fb05ef9521
Justin Fang <231964261+justinfang37@users.noreply.github.com>
2026-05-17 18:14:40 -0400

    Update control_node.hpp

--- src/robot/control/include/control_node.hpp
+++ src/robot/control/include/control_node.hpp
@@ -2,15 +2,39 @@
 #define CONTROL_NODE_HPP_
 
 #include "rclcpp/rclcpp.hpp"
-
+#include "nav_msgs/msg/path.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
 #include "control_core.hpp"
+#include <cmath>
+#include <optional>
 
 class ControlNode : public rclcpp::Node {
   public:
     ControlNode();
 
   private:
-    robot::ControlCore control_;
+    rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr path_sub_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    nav_msgs::msg::Path::SharedPtr current_path_;
+    nav_msgs::msg::Odometry::SharedPtr robot_odom_;
+
+    double lookahead_distance_ = 1.0;
+    double goal_tolerance_ = 0.5;
+    double linear_speed_ = 0.5;
+
+    void pathCallback(const nav_msgs::msg::Path::SharedPtr msg);
+    void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg);
+    void timerCallback();
+
+    std::optional<geometry_msgs::msg::PoseStamped> findLookaheadPoint();
+    geometry_msgs::msg::Twist computeVelocity(const geometry_msgs::msg::PoseStamped &target);
+    double computeDistance(const geometry_msgs::msg::Point &a, const geometry_msgs::msg::Point &b);
+    double extractYaw(const geometry_msgs::msg::Quaternion &q);
 };
 
 #endif
