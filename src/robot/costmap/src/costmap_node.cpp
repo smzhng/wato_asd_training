66743de43dd38d5d1590a4a081ae8a2d00c78751
Justin Fang <231964261+justinfang37@users.noreply.github.com>
2026-05-17 16:26:22 -0400

    Update costmap_node.cpp

--- src/robot/costmap/src/costmap_node.cpp
+++ src/robot/costmap/src/costmap_node.cpp
@@ -4,14 +4,14 @@
 #include <vector>
 #include "costmap_node.hpp"
  
-CostmapNode::CostmapNode() : Node("costmap"), costmap_(robot::CostmapCore(this->get_logger())) {
+CostmapNode::CostmapNode() : Node("costmap"), costmap_core_(robot::CostmapCore(this->get_logger())) {
   // Initialize the constructs and their parameters
     lidar_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>("/lidar", 10, std::bind(&CostmapNode::laserCallback, this, std::placeholders::_1));
     costmap_pub_ = this->create_publisher<nav_msgs::msg::OccupancyGrid>("/costmap",10);
 }
  
 // Define the timer to publish a message every 500ms
-void CostmapNode::laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+void CostmapNode::laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr scan) {
     float angle_min = scan->angle_min; //just takes in the lidar outputs
     //technically don tneed angle max if i use increments?
     float angle_increment = scan->angle_increment;
@@ -22,7 +22,7 @@ void CostmapNode::laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg
     int count = ranges.size(); //basically how many scans it takes
 
     // required grid size to capture all the lidar signals
-    int range_size = static_cast<int>(std::cell((2.0*range_max)/resolution))+1; //static cast converts it to int basically
+    int range_size = static_cast<int>(std::ceil((2.0*range_max)/resolution))+1; //static cast converts it to int basically
     int origin = range_size/2; //tells the robot its in the very center in reference to the lidar scanners
 
     //makes the grid, in this case, there are range_size # of rows and columns, all filled with 0 for now. does it at the start of every call to start fresh (NECESSARY)
@@ -36,8 +36,8 @@ void CostmapNode::laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg
             continue; //basically skipping the reading if it's invalid
         }
         //distance*angle to find the x,y coordinates, then adds to the origin to show the point assuming origin is where car is at for its own scan
-        int x = static_cast<int>((ranges*std::sin(angle))/resolution)+origin;
-        int y = static_cast<int>((ranges*std::cos(angle))/resolution)+origin;
+        int x = static_cast<int>((range*std::sin(angle))/resolution)+origin;
+        int y = static_cast<int>((range*std::cos(angle))/resolution)+origin;
 
         if (x<0 || x>=range_size||y<0||y>=range_size){
             continue; //like the if statement above, skipping if the x and y are out of bounds
@@ -55,7 +55,7 @@ void CostmapNode::laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg
 
                 float dist = std::sqrt(dx*dx+dy*dy)*resolution; //pythagorean to find distance
                 if (nx>=0 && nx<range_size && ny >=0 && ny<range_size && dist<=inflation_radius){
-                    int inflated_cost = static_cast<int>(occupied*(1.0*-dist/inflation_radius));
+                    int inflated_cost = static_cast<int>(occupied*(1.0-dist/inflation_radius));
 
                     costmap_[ny][nx] = std::max(costmap_[ny][nx], inflated_cost); //assigns the inflated costs to the tiles
                 }
