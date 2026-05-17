102de8f9391efb1903af64693e7fd7c4552fd03c
Justin Fang <231964261+justinfang37@users.noreply.github.com>
2026-05-17 15:17:48 -0400

    Update costmap_node.cpp

--- src/robot/costmap/src/costmap_node.cpp
+++ src/robot/costmap/src/costmap_node.cpp
@@ -1,20 +1,39 @@
 #include <chrono>
 #include <memory>
-
+ 
+#include <vector>
 #include "costmap_node.hpp"
-
+ 
 CostmapNode::CostmapNode() : Node("costmap"), costmap_(robot::CostmapCore(this->get_logger())) {
-  string_pub_ = this->create_publisher<std_msgs::msg::String>("/test_topic", 10);
-  timer_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&CostmapNode::publishMessage, this));
+  // Initialize the constructs and their parameters
+    lidar_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>("/lidar", 10, std::bind(&CostmapNode::laserCallback, this, std::placeholders::_1));
+    costmap_pub_ = this->create_publisher<nav_msgs::msg::OccupancyGrid>("/costmap",10);
+}
+ 
+// Define the timer to publish a message every 500ms
+void CostmapNode::laserCallback(const sensor_msgs::msg::LaserScan::SharePtr msg) {
+    float angle_min = scan->angle_min; //just takes in the lidar outputs
+    float angle_max = scan->angle_max;
+    float angle_increment = scan->angle_increment;
+    float range_min = scan->range_min;
+    float range_max = scan->range_max;
+
+    const auto &ranges = scan->ranges; //creates alias for the ranges
+    int count = ranges.size();
+  
 }
 
-void CostmapNode::publishMessage() {
-  auto message = std_msgs::msg::String();
-  message.data = "Hello, ROS 2!";
-  RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-  string_pub_->publish(message);
+void CostmapNode::publishCostmap(int width, int origin, float resolution){
+    nav_msgs::msg::OccupancyGrid grid;
+    grid.header.stamp= this->now();
+    grid.header.frame_id = "map";
+    grid.info.resolution = resolution;
+    grid.info.width = width;
+    grid.info.height = width; //square kinda
+    
+    
 }
-
+ 
 int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
