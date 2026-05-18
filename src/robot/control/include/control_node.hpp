b63aba7111979cf2e8bd0a85f51698618b5774c6
Justin Fang <231964261+justinfang37@users.noreply.github.com>
2026-05-17 21:36:09 -0400

    Update control_node.hpp

--- src/robot/control/include/control_node.hpp
+++ src/robot/control/include/control_node.hpp
@@ -1,40 +1,41 @@
 #ifndef CONTROL_NODE_HPP_
 #define CONTROL_NODE_HPP_
 
-#include "rclcpp/rclcpp.hpp"
-#include "nav_msgs/msg/path.hpp"
-#include "nav_msgs/msg/odometry.hpp"
-#include "geometry_msgs/msg/twist.hpp"
-#include "geometry_msgs/msg/pose_stamped.hpp"
-#include "control_core.hpp"
+#include <rclcpp/rclcpp.hpp>
+#include <nav_msgs/msg/path.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <geometry_msgs/msg/twist.hpp>
 #include <cmath>
 #include <optional>
 
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <memory>
+
+
 class ControlNode : public rclcpp::Node {
-  public:
+public:
     ControlNode();
 
-  private:
+private:
+
+    // ROS shit
     rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr path_sub_;
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
-    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::TimerBase::SharedPtr control_timer_;
 
     nav_msgs::msg::Path::SharedPtr current_path_;
     nav_msgs::msg::Odometry::SharedPtr robot_odom_;
 
-    double lookahead_distance_ = 1.0;
-    double goal_tolerance_ = 0.5;
-    double linear_speed_ = 2.0;
+    double lookahead_distance_;
+    double goal_tolerance_;
+    double linear_speed_;
 
-    void pathCallback(const nav_msgs::msg::Path::SharedPtr msg);
-    void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg);
-    void timerCallback();
-
+    void controlLoop();
     std::optional<geometry_msgs::msg::PoseStamped> findLookaheadPoint();
     geometry_msgs::msg::Twist computeVelocity(const geometry_msgs::msg::PoseStamped &target);
     double computeDistance(const geometry_msgs::msg::Point &a, const geometry_msgs::msg::Point &b);
-    double extractYaw(const geometry_msgs::msg::Quaternion &q);
+    double extractYaw(const geometry_msgs::msg::Quaternion &quat);
 };
 
-#endif
+#endif // CONTROL_NODE_HPP_
