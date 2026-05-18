bf9e69d9e5e1e9a43af0c2af2cf51dc06833527a
smzhng <samzhng07@gmail.com>
2026-05-17 21:20:38 -0400

    replace core files with correct implementation

--- src/robot/costmap/include/costmap_core.hpp
+++ src/robot/costmap/include/costmap_core.hpp
@@ -3,19 +3,41 @@
 
 #include "rclcpp/rclcpp.hpp"
 
+#include <vector>
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
+
 namespace robot
 {
 
-class CostmapCore {
+  class CostmapCore
+  {
   public:
-    // Constructor, we pass in the node's RCLCPP logger to enable logging to terminal
-    explicit CostmapCore(const rclcpp::Logger& logger);
+    explicit CostmapCore(const rclcpp::Logger &logger);
+
+    void configure(double resolution, int width, int height,
+                   double inflation_radius, int max_cost);
+
+    void processScan(const sensor_msgs::msg::LaserScan &scan);
+
+    nav_msgs::msg::OccupancyGrid getCostmap() const;
 
   private:
     rclcpp::Logger logger_;
+    std::vector<int8_t> grid_;
+    double resolution_;
+    int width_;
+    int height_;
+    double origin_x_;
+    double origin_y_;
+    double inflation_radius_;
+    int max_cost_;
 
-};
+    bool worldToGrid(double x, double y, int &gx, int &gy) const;
+    void markObstacle(int gx, int gy);
+    void inflateObstacles();
+  };
 
-}  
+}
 
-#endif  
+#endif
--- src/robot/map_memory/include/map_memory_core.hpp
+++ src/robot/map_memory/include/map_memory_core.hpp
@@ -2,18 +2,38 @@
 #define MAP_MEMORY_CORE_HPP_
 
 #include "rclcpp/rclcpp.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
 
+#include <vector>
+#include <string>
 namespace robot
 {
 
-class MapMemoryCore {
+  class MapMemoryCore
+  {
   public:
-    explicit MapMemoryCore(const rclcpp::Logger& logger);
+    explicit MapMemoryCore(const rclcpp::Logger &logger);
+
+    void configure(double resolution, int width, int height,
+                   const std::string &frame_id);
+
+    void integrate(const nav_msgs::msg::OccupancyGrid &local,
+                   double robot_x, double robot_y, double robot_yaw);
+
+    nav_msgs::msg::OccupancyGrid getMap() const;
 
   private:
     rclcpp::Logger logger_;
-};
+    std::vector<int8_t> grid_;
+    double resolution_;
+    int width_;
+    int height_;
+    double origin_x_;
+    double origin_y_;
+    std::string frame_id_;
 
-}  
+    bool worldToGrid(double x, double y, int &gx, int &gy) const;
+  };
+}
 
-#endif  
+#endif
